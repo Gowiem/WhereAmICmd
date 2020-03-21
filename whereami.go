89d1adc071013958416fc823e5042d134c9dc914
Matt Gowie <matt@masterpoint.io>
2020-03-21 13:04:26 -0600

    Fixes LocationResponse struct formatting

--- whereami.go
+++ whereami.go
@@ -9,20 +9,20 @@ import (
 )
 
 type LocationResponse struct {
-    Status string `json:"status"`
-    Country string `json:"country"`
-    CountryCode string `json:"countryCode"`
-		Region string `json:"region"`
-		RegionName string `json:"regionName"`
-		City string `json:"city"`
-		Zip string `json:"zip"`
-		Lat float32 `json:"lat"`
-		Lon float32 `json:"lon"`
-		Timezone string `json:"timezone"`
-		Isp string `json:"isp"`
-		Org string `json:"org"`
-		As string `json:"as"`
-		Query string `json:"query"`
+	Status string `json:"status"`
+	Country string `json:"country"`
+	CountryCode string `json:"countryCode"`
+	Region string `json:"region"`
+	RegionName string `json:"regionName"`
+	City string `json:"city"`
+	Zip string `json:"zip"`
+	Lat float32 `json:"lat"`
+	Lon float32 `json:"lon"`
+	Timezone string `json:"timezone"`
+	Isp string `json:"isp"`
+	Org string `json:"org"`
+	As string `json:"as"`
+	Query string `json:"query"`
 }
 
 type LocationResult struct {

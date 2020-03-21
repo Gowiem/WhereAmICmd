25d70f5df5c19d85e88f0ecfc937043d288e14f6
Matt Gowie <matt@masterpoint.io>
2020-03-21 12:55:17 -0600

    Adds initial cut at location tracking in go

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Gowiem/WhereAmICmd
 
 go 1.14
+
+require (
+	github.com/alexkohler/nakedret v1.0.0 // indirect
+	github.com/go-resty/resty/v2 v2.2.0
+	github.com/kisielk/errcheck v1.2.0 // indirect
+	github.com/kisielk/gotool v1.0.0 // indirect
+	github.com/mdempsky/maligned v0.0.0-20180708014732-6e39bd26a8c8 // indirect
+	github.com/stripe/safesql v0.2.0 // indirect
+	github.com/walle/lll v1.0.1 // indirect
+	golang.org/x/tools v0.0.0-20200321014904-268ba720d32c // indirect
+)
---
+++ whereami.go
@@ -0,0 +1,90 @@
+package main
+// Command line application to POST location information to the WhereAmI Server
+
+import (
+	"fmt"
+	"log"
+	"time"
+	// "encoding/json"
+	"github.com/go-resty/resty/v2"
+)
+
+type LocationResponse struct {
+    Status string `json:"status"`
+    Country string `json:"country"`
+    CountryCode string `json:"countryCode"`
+		Region string `json:"region"`
+		RegionName string `json:"regionName"`
+		City string `json:"city"`
+		Zip string `json:"zip"`
+		Lat float32 `json:"lat"`
+		Lon float32 `json:"lon"`
+		Timezone string `json:"timezone"`
+		Isp string `json:"isp"`
+		Org string `json:"org"`
+		As string `json:"as"`
+		Query string `json:"query"`
+}
+
+type LocationResult struct {
+	CountryLong string `json:"country_long"`
+	Country string `json:"country"`
+	City string `json:"city"`
+	Region string `json:"region"`
+	Lat float32 `json:"latitude"`
+	Lon float32 `json:"longitude"`
+	Timestamp int32 `json:"timestamp"`
+	Ip string `json:"ip"`
+}
+
+// Uploads the user's approximate location to whereami.mattgowie.com
+//
+// 1. Fetches the client's IP Address from icanhazip.com
+// 2. Does a lookup of the fetched IP Address from the ip-api.com API
+// 3. POSTs the client's location infomration to whereami.mattgowie.com
+func main() {
+	client := resty.New()
+
+	ip_resp, err := client.R().Get("https://icanhazip.com")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("IP Address: ", ip_resp)
+
+
+	location_url := fmt.Sprintf("http://ip-api.com/json/%s", ip_resp)
+	location_resp, location_err := client.R().SetResult(LocationResponse{}).Get(location_url)
+
+	if location_err != nil {
+		log.Fatal(location_err)
+	}
+
+	fmt.Println("Location Response: ", location_resp)
+
+	var location = location_resp.Result().(*LocationResponse)
+
+	var result = LocationResult {
+		CountryLong: location.Country,
+		Country    : location.CountryCode,
+		City       : location.City,
+		Region     : location.Region,
+		Lat        : location.Lat,
+		Lon        : location.Lon,
+		Timestamp  : int32(time.Now().Unix()),
+		Ip         : fmt.Sprintf("%s", ip_resp) }
+
+	fmt.Println("Result: ", result)
+
+	post_resp, post_err := client.R().
+      SetBody(result).
+      Post("https://docker-micro.mattgowie.com/location")
+
+	if post_err != nil {
+		log.Fatal(post_err)
+	}
+
+	fmt.Println("Location Update Response: ", post_resp)
+	fmt.Println("Completed successfully!")
+}
